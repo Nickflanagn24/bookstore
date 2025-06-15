10bd9bd19a7f0b6855e14ccd3a6a53fd5cec2a49
Nickflanagn24 <lpao26ad@students.codeinstitute.net>
2025-06-15 21:13:52 +0000

    Add SEO foundation and meta tag optimization
    - Create robots.txt template with development/production logic
    - Add comprehensive meta tags for search engines
    - Implement Open Graph and Twitter Card meta tags
    - Set up SEO-friendly URL structure for future sitemap
    - Add proper robots meta directives
    - Prepare custom error handler structure
    - Enhance page titles and descriptions for better indexing

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -6,16 +6,42 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.contrib.sitemaps.views import sitemap
+
+# Import sitemaps when they're created
+# from .sitemaps import BookSitemap, StaticViewSitemap
+
+# sitemaps = {
+#     'books': BookSitemap,
+#     'static': StaticViewSitemap,
+# }
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    # Temporary home page until main app is created
+    # Main app URLs (to be added as we develop)
+    # path('', include('books.urls')),
+    # path('accounts/', include('accounts.urls')),
+    # path('orders/', include('orders.urls')),
+    # path('reviews/', include('reviews.urls')),
+    
+    # Home page
     path('', TemplateView.as_view(template_name='base.html'), name='home'),
     
-    # SEO URLs (will be implemented later)
-    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'), name='robots'),
+    # SEO URLs
+    path('robots.txt', TemplateView.as_view(
+        template_name='robots.txt', 
+        content_type='text/plain'
+    ), name='robots'),
+    
+    # Sitemap (will be activated when we have content)
+    # path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, 
+    #      name='django.contrib.sitemaps.views.sitemap'),
 ]
+
+# Custom error handlers (to be implemented)
+# handler404 = 'books.views.custom_404'
+# handler500 = 'books.views.custom_500'
 
 # Serve media files in development
 if settings.DEBUG:
