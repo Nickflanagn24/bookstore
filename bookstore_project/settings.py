da2de8d668dfeaebbfeba210140263b8170b90f4
Nickflanagn24 <lpao26ad@students.codeinstitute.net>
2025-07-14 21:57:52 +0000

    Fix STATICFILES_DIRS to work in production

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -125,9 +125,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = []
-if DEBUG:
-    STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Media files
